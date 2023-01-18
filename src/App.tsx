8b1e5266b0e482c8297c2eb78e561761b1d9dc82
RMerkulov <fotoromario@gmail.com>
2023-01-18 09:46:11 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="text-3xl font-bold underline text-red-600">
+            Hello world!
+        </h1>
+    </div>
+  );
+}
+
+export default App;
