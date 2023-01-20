01578bb219c93d6546e1902d19ea09d85ba677a2
RMerkulov <fotoromario@gmail.com>
2023-01-20 11:52:04 +0200

    update

---
+++ src/components/TrainingItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrainingItem = () => {
+  return <div className="bg-amber-50 w-screen">Test</div>;
+};
+
+export default TrainingItem;
--- src/views/TrainingPage.tsx
+++ src/views/TrainingPage.tsx
@@ -1,11 +1,113 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const TrainingPage = () => {
-    return (
-        <div>
+  const [trainName, setTrainName] = useState("");
+  const [repeat, setRepeat] = useState("");
+  const [sets, setSets] = useState("");
+  const [fill, setFill] = useState("bad");
 
-        </div>
-    );
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    switch (e.target.name) {
+      case "exercise":
+        setTrainName(e.target.value);
+        break;
+      case "repeats":
+        setRepeat(e.target.value);
+        break;
+      case "sets":
+        setSets(e.target.value);
+        break;
+      case "bad":
+        setFill(e.target.value);
+        break;
+      case "medium":
+        setFill(e.target.value);
+        break;
+      case "good":
+        setFill(e.target.value);
+        break;
+    }
+  };
+
+  return (
+    <div className="bg-main bg-no-repeat bg-cover  bg-center bg-fixed w-screen h-screen">
+      <div className="bg-white bg-opacity-20 backdrop-blur-md rounded drop-shadow-lg w-screen h-screen flex items-start justify-center ">
+        <form className="flex flex-col items-center justify-center gap-4 mt-[100px] w-[300px]">
+          <label>
+            <input
+              className="p-1 rounded-md border-2 border-amber-300 w-[300px] "
+              type="text"
+              name="exercise"
+              value={trainName}
+              placeholder="Enter name of exercises"
+              onChange={handleChange}
+            />
+          </label>
+          <div className="flex items-center justify-start gap-3">
+            <label>
+              <input
+                className="p-1 rounded-md border-2 border-amber-300 w-[80px]"
+                type="text"
+                name="repeats"
+                value={repeat}
+                placeholder="repeats"
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              <input
+                className="p-1 rounded-md border-2 border-amber-300 w-[80px]"
+                type="text"
+                name="sets"
+                value={sets}
+                placeholder="sets"
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+          <div className="flex items-center justify-start gap-3">
+            <label>
+              <input
+                type="radio"
+                id="chooseBad"
+                name="bad"
+                value="bad"
+                checked={fill === "bad"}
+                onChange={handleChange}
+              />{" "}
+              bad
+            </label>
+            <label>
+              <input
+                type="radio"
+                id="chooseMedium"
+                name="medium"
+                value="medium"
+                checked={fill === "medium"}
+                onChange={handleChange}
+              />
+              medium
+            </label>
+            <label>
+              <input
+                type="radio"
+                id="chooseGood"
+                name="good"
+                value="good"
+                checked={fill === "good"}
+                onChange={handleChange}
+              />
+              good
+            </label>
+          </div>
+          <button className="block mx-auto rounded-md px-[50px] py-1 bg-amber-100 text-[20px] font-bold text-gray-700 shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-amber-300 ">
+            done
+          </button>
+        </form>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default TrainingPage;
