7c0f51148692fef5daf6428448bda935dc0d8422
RMerkulov <fotoromario@gmail.com>
2023-01-23 16:51:46 +0200

    commit

--- src/components/TrainingItem.tsx
+++ src/components/TrainingItem.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 type DataProps = {
-  data: () => void;
+  data: {};
 };
 
-const TrainingItem = ({ data }: DataProps) => {
+const TrainingItem: React.FC<DataProps> = ({ data }) => {
+  console.log(data);
   return (
     <div className="bg-amber-50 w-full shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]">
-      Test
+      {data}
     </div>
   );
 };
