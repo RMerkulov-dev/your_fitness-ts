3e019ed45ce8f3873fe152d7bfbb477a397bf02a
RMerkulov <fotoromario@gmail.com>
2023-01-24 13:11:16 +0200

    card

--- src/components/TrainingItem.tsx
+++ src/components/TrainingItem.tsx
@@ -12,9 +12,17 @@ export interface Trains {
 }
 
 const TrainingItem = ({ trains }: TrainsProps) => {
-  console.log(trains);
   return (
-    <div className="bg-amber-50 w-full shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]"></div>
+    <>
+      {trains.map((train) => (
+        <li className="bg-amber-50 w-full shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]">
+          <span>{train.trainName}</span>
+          <span>{train.repeat}</span>
+          <span>{train.sets}</span>
+          <span>{train.fill}</span>
+        </li>
+      ))}
+    </>
   );
 };
 
