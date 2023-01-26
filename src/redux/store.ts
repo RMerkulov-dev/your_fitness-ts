de9b7bc05898fa761826b96369d17b476c7631e9
RMerkulov <fotoromario@gmail.com>
2023-01-26 17:49:08 +0200

    add persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import TrainsReducer from "../redux/AllTrainsSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfigTrains = {
+  key: "auth",
+  storage,
+};
 
 const store = configureStore({
   reducer: {
-    train: TrainsReducer,
+    train: persistReducer(persistConfigTrains, TrainsReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
